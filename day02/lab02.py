ff8974669cf66b8538961c39c050bcee82f315ce
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-08-07 14:54:41 -0500

    welp, thats what I got

--- day02/lab02.py
+++ day02/lab02.py
@@ -7,17 +7,66 @@ class Clock(object):
 
     ## Print the time
     def __str__(self):
+        if self.minutes==0:
+            displaymins="00"
+        elif self.minutes<10:
+            displaymins="0%s"%self.minutes
+        else:
+            displaymins=str(self.minutes)
+        if self.hour==0:
+            displayhours="00"
+        elif self.hour<10:
+            displayhours="0%s"%self.hour
+        else:
+            displayhours=str(self.hour)
+        return "%s:%s" %(displayhours,displaymins)
     
     ## Add time
     ## Don't return anything
     def __add__(self,minutes):
-    
+        newtime=self.minutes+minutes
+        houradd=newtime/60
+        newmins=newtime%60
+        self.minutes=newmins
+        if self.hour+houradd>=24:
+            overdays=(self.hour+houradd)/24
+            self.hour=self.hour+houradd-(24*overdays)
+        else:
+            self.hour=self.hour+houradd
+
     ## Subtract time
     ## Don't return anything
     def __sub__(self,minutes):
-    
+        if self.minutes-minutes<0 and self.minutes-minutes>-60:
+            newtime=60-abs(self.minutes-minutes)
+            hoursub=(newtime/60)+1
+        elif self.minutes-minutes<-60:
+            hoursub=abs((self.minutes-minutes)/60)
+            newtime=60-abs(self.minutes-minutes)%60
+        else:
+            newtime=self.minutes-minutes
+            hoursub=newtime/60
+        self.minutes=newtime
+        if hoursub-self.hour>=24:
+            overdays=(hoursub-self.hour)/24
+            self.hour=hoursub-self.hour-(24*overdays)
+        else:
+            self.hour=self.hour-hoursub
+
     ## Are two times equal?
     def __eq__(self, other):
+        if self.minutes==other.minutes and self.hour==other.hour:
+            print "TRUE"
+        else:
+            print "FALSE"
     
     ## Are two times not equal?
     def __ne__(self, other):
+        if self.minutes!=other.minutes and self.hour!=other.hour:
+            print "TRUE"
+        else:
+            print "FALSE"
+
+lit=Clock(12,30)
+lit-25
+print lit
