6a14ef3cdcf33a921e69f1c733d1f81be994180f
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-08-09 11:48:03 -0500

    fixed selling functionality and adapted for mutual funds

--- homeworks/hw1/BRS_hw1.py
+++ homeworks/hw1/BRS_hw1.py
@@ -1,3 +1,6 @@
+import datetime
+now = datetime.datetime.now()
+
 
 class Portfolio(object):
 	def __init__(self,client):
@@ -8,11 +11,13 @@ class Portfolio(object):
 	def __str__(self):
 		return "The portfolio of %s contains: \n %s" %(self.client,self.contents)
 	
-	def addCash(self,cash,daytime):
+	def addCash(self,cash):
 		self.contents["cash"]+=cash
+		daytime=now.strftime("%B-%d-%Y %H:%M:%S")
 		self.log.append("On %s %s added %i in cash to their portfolio. Balance at time of transaction: %i" % (daytime,self.client,cash,self.contents["cash"]))
 
-	def withdrawCash(self,cash,daytime):
+	def withdrawCash(self,cash):
+		daytime=now.strftime("%B-%d-%Y %H:%M:%S")
 		if self.contents["cash"]-cash<0:
 			self.log.append("On %s %s attempted to withdraw %i in cash from their portfolio, but were denied due to insufficient funds. Balance at time of transaction: %i" % (daytime,self.client,cash,self.contents["cash"]))
 			return "Insufficient funds. Must move around assets to remove %i in cash from account. Current balance: %i" % (cash,self.contents["cash"])
@@ -23,29 +28,53 @@ class Portfolio(object):
 	def history(self):
 		return self.log
 
-	def buyStock(self,name,daytime,quantity):
+	def buyStock(self,name,quantity):
+		daytime=now.strftime("%B-%d-%Y %H:%M:%S")
 		if name.price*quantity>self.contents["cash"]:
 			self.log.append("On %s, %s attempted to purchase %i unit(s) of the stock %s. %i was needed for this transation. Balance at time of transaction: %i" %(daytime,self.client,quantity,name.stock_name,name.price*quantity,self.contents["cash"]))
-			return "Insufficent funds to %i of stock %s. %i needed for this transaction. Current balance: %i"%(quantity,name.stock_name,name.price*quantity,self.contents["cash"])
+			return "Insufficent funds to purchase %i of stock %s. %i needed for this transaction. Current balance: %i"%(quantity,name.stock_name,name.price*quantity,self.contents["cash"])
+		#elif self.contents["stocks"]["%s"%name.stock_name][1]>0:
+		#	self.contents["cash"]-=name.price*quantity
+		#	self.contents["stocks"].update({"%s"%name.stock_name:[name.price,quantity+self.contents["stocks"]["%s"%name.stock_name][1]]})
+		#	self.log.append("On %s, %s purchased %i unit(s) of the stock %s for %i. Cash balance after transaction: %i"%(daytime,self.client,quantity,name.stock_name,name.price*quantity,self.contents["cash"]))
 		else:
 			self.contents["cash"]-=name.price*quantity
 			self.contents["stocks"].update({"%s"%name.stock_name:[name.price,quantity]})
 			self.log.append("On %s, %s purchased %i unit(s) of the stock %s for %i. Cash balance after transaction: %i"%(daytime,self.client,quantity,name.stock_name,name.price*quantity,self.contents["cash"]))
 
-	def sellStock(self,name,daytime,quantity):
+	def sellStock(self,name,quantity):
+		daytime=now.strftime("%B-%d-%Y %H:%M:%S")
 		if self.contents["stocks"]["%s"%name][1]<quantity:
+			self.log.append("On %s, %s attempted to sell %i unit(s) of the stock %s. They only had %i unit(s) of the stock." %(daytime,self.client,quantity,name,self.contents["stocks"]["%s"%name][1]))
 			return "You are attempting to sell more stocks than you have"
 		else:
 			newprice=random.uniform(self.contents["stocks"]["%s"%name][0]*.5,self.contents["stocks"]["%s"%name][0]*1.5)
-			self.contents["stocks"].update({"%s"%name.stock_name:[name.price,0]})
+			newstock={"%s"%name:[self.contents["stocks"]["%s"%name][0],self.contents["stocks"]["%s"%name][1]-quantity]}
+			self.contents["stocks"].update(newstock)
 			self.contents["cash"]+=newprice*quantity
-			return self.contents["stocks"]["%s"%name.stock_name][1]-quantity
+			self.log.append("On %s, %s sold %i unit(s) of the stock %s. The original price was %i and the new price was %r. Cash Balance at time of transation: %r" %(daytime,self.client,quantity,name,self.contents["stocks"]["%s"%name][0],newprice,self.contents["cash"]))
 
-	#def buyMutualFund(self):
+	def buyMutualFund(self,fund,quantity):
+		daytime=now.strftime("%B-%d-%Y %H:%M:%S")
+		if quantity>self.contents["cash"]:
+			self.log.append("On %s, %s attempted to purchase %i unit(s) of the mutual fund %s. $%i was needed for this transation. Balance at time of transaction: %i" %(daytime,self.client,quantity,fund.name,quantity,self.contents["cash"]))
+			return "Insufficent funds to purchase %i of mutual fund %s. $%i needed for this transaction. Current balance: %i"%(quantity,fund.name,quantity,self.contents["cash"])
+		else:
+			self.contents["cash"]-=quantity
+			self.contents["mutual funds"].update({"%s"%fund.name:[quantity]})
+			self.log.append("On %s, %s purchased %i unit(s) of the mutual fund %s. Cash balance after transaction: %r"%(daytime,self.client,quantity,fund.name,self.contents["cash"]))
 
-	#def sellMutualFund(self):
-
-	#def sellStock(self):
+	def sellMutualFund(self,name,quantity):
+		daytime=now.strftime("%B-%d-%Y %H:%M:%S")
+		if self.contents["mutual funds"]["%s"%name][0]<quantity:
+			self.log.append("On %s, %s attempted to sell %i unit(s) of the mutual fund %s. They only had %i unit(s) of the stock." %(daytime,self.client,quantity,name,self.contents["mutual funds"]["%s"%name][0]))
+			return "You are attempting to sell more mutual funds than you have"
+		else:
+			newprice=random.uniform(.9,1.2)
+			newfund={"%s"%name:[self.contents["mutual funds"]["%s"%name][0]-quantity]}
+			self.contents["mutual funds"].update(newfund)
+			self.contents["cash"]+=newprice*quantity
+			self.log.append("On %s, %s sold %i unit(s) of the mutual fund %s. The original price was $1 and the new price was %r. Cash Balance at time of transation: %r" %(daytime,self.client,quantity,name,newprice,self.contents["cash"]))
 
 
 class Stock(object):
@@ -56,23 +85,28 @@ class Stock(object):
 	def __str__(self):
 		return "The stock, %s, has a price of %i" %(self.stock_name,self.price)
 
-#class MutualFund(object):
-#	def __init__(self,stock_name):
-#		self.stock=stock_name
+class MutualFund(object):
+	def __init__(self,fund_name):
+		self.name=fund_name
 
 
 
 #test code
 
 ben=Portfolio("Benjamin")
-ben.addCash(50, "Aug 8, 3:56pm")
-print ben
-
-#ben.withdrawCash(100, "Aug 8, 4:04pm")
-#ben.withdrawCash(35, "Aug 8, 4:05pm")
-ben.history()
+ben.addCash(50)
+ben.withdrawCash(100)
+ben.withdrawCash(35)
+ben.addCash(150)
 newstock=Stock(30,"APPL")
-ben.buyStock(newstock,"Aug 8 4:30pm",2)
-ben.buyStock(newstock,"Aug 8 4:30pm",1)
-ben.sellStock("APPL","Aug 8, 4:41pm",1)
+ben.buyStock(newstock,10)
+ben.buyStock(newstock,3)
+ben.sellStock("APPL",4)
+ben.sellStock("APPL",2)
+newfund=MutualFund("Lit")
+ben.buyMutualFund(newfund,200)
+ben.buyMutualFund(newfund,50)
+ben.sellMutualFund("Lit",50)
+
+
 
