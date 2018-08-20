a7f1c7309ed13faad69cf421d0a5c254081ad1f7
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-08-20 13:32:20 -0500

    commit
    I need to still comment the code, but this is done for the most aprt

--- homeworks/hw3/HW3_BRS.py
+++ homeworks/hw3/HW3_BRS.py
@@ -14,6 +14,67 @@ for g in stlgroups:
 
 x=max(memberlist)
 
+for g in stlgroups:
+	if g.members==x:
+		namesave=g.urlname
+	else:
+		pass
+
+populargroup=api.GetMembers({"group_urlname" : namesave.encode("utf-8")})
+
+people=populargroup.__dict__["results"]
+
+memberid=[]
+for member in people:
+	memberid.append(member["id"])
+
+groupnum=[]
+for i in memberid:
+	mygroups = api.GetGroups({"member_id" : i})
+	groupnum.append(mygroups.results)
+
+groupmembership=[]
+for i in groupnum:
+	groupmembership.append(len(i))
+
+for i in range(len(groupmembership)):
+	if len(groupnum[i])==58:
+		print i
+
+people[101] #this is our Mr. Popular Alf
+
+groupurls=[]
+for g in stlgroups:
+	groupurls.append(g.urlname)
+
+activemembers=[]
+for i in groupurls:
+	group=api.GetMembers({"group_urlname" : i.encode("utf-8")})
+	activemembercount=0
+	for i in range(len(group.results)):
+		if group.results[i]['status']=='active':
+			activemembercount+=1
+		else:
+			pass
+	activemembers.append(activemembercount)
+
+maxactive=max(activemembers)
+
+for i in range(len(activemembers)):
+	if activemembers[i]==maxactive:
+		print stlgroups[i].name
+
+#Looks like there are a lot of groups with 200 active users. Because we can only scrape that many, that is the best I can do
 
 
-stlgroups[0].__dict__.keys()
+
+
+
+
+
+
+
+
+
+
+
